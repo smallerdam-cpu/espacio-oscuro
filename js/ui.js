58b8c770c27abdd17e9649013b25ec02765117b3
Roberto Smith <smallerdam@gmail.com>
2026-03-25 13:08:54 -0500

    Create js/ui.js with all UI components

---
+++ js/ui.js
@@ -0,0 +1,25 @@
+// ui.js
+
+// UI Menus
+function createMainMenu() {
+    // Code for creating the main menu
+}
+
+function createSettingsMenu() {
+    // Code for creating the settings menu
+}
+
+// Player Profile
+function displayPlayerProfile(player) {
+    // Code for displaying player profile
+}
+
+// HUD (Heads-Up Display)
+function updateHUD(playerStats) {
+    // Code for updating the HUD
+}
+
+// Minimap
+function initializeMinimap() {
+    // Code for initializing the minimap
+}
